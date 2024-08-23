b13a9fe8636992cb18c6444ab8c6205cb8d14267
jumbocar <jacobblake1112@yahoo.com>
2024-08-23 12:03:44 -0500

    Exercises finished

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -10,23 +10,31 @@ flavors = {
 }
 
 ## Set a variable called choice to the flavor you want to search for.
-
+choice = 'mint chocolate chip'
 ## Use an if statement to check if choice is in the flavors dictionary.
-
+print(choice in flavors)
 ## If it is, set another variable called cost to the value associated with choice.
+if choice in flavors:
+    cost = flavors[choice]
+    
 
 ## If it isn’t, set cost to 0.
-
+if choice not in flavors:
+    cost = 0
 ## Print the cost.
-
+print(cost)
 ### Search a Dictionary Part 2:
 
 ## Initialize two variables: highest_cost to 0 and fanciest to an empty string.
-
+highest_cost = 0
+fanciest = ''
 ## Loop through the flavors dictionary using a for loop.
-
+for key in flavors:
 ## For each flavor, check if its price is higher than highest_cost.
-
+    if flavors[key] > highest_cost:
 ## If it is, update fanciest to this flavor and highest_cost to its price.
-
+      fanciest = key
+      highest_cost = flavors[key]
 ## After the loop, print the most expensive flavor.
+print(f'''Most epensive flavor is {fanciest}
+Highest cost is {highest_cost}''')
