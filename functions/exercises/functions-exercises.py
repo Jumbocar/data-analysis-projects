fc639d850b026b1fec6954c09085986579b84c3b
jumbocar <jacobblake1112@yahoo.com>
2024-08-29 10:55:40 -0500

    Part 1+2A

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -1,22 +1,44 @@
 # Part 1 A -- Make a Line
-print(line(5))
+def make_line(num):
+    line = ''
+    for i in range(num):
+        line += '#'
+    return line
+
+# print(make_line(5))
 
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
 
+def make_square(num):
+    column = ''
+    for x in range(num):
+        column += '\n#' + make_line(num)
+    return column
 
-
-
+# print(make_square(5))
 
 # Part 1 C -- Make a Rectangle
 
+def make_rectangle(length,width):
+    rec = ''
+    for x in range(length):
+        rec += "\n#" + make_line(width) 
+    return rec
 
 
+# print(make_rectangle(5,3))
 
 
 # Part 2 A -- Make a Stairs
+def make_stairs(steps):
+    stairs = '' 
+    for x in range(steps):
+        stairs += '\n#'+ make_line(x)
+    return stairs
 
+print(make_stairs(6))  
 
 
 
